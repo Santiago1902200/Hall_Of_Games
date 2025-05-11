7128b58ea1b26ce3e56a549a6a8ad7ddfc236790
Al3J077 <garciaalejo711@gmail.com>
2025-05-10 22:22:51 -0500

    Delete hallofgames directory

--- hallofgames/views/js/showpss.js
+++
@@ -1,9 +0,0 @@
-function mostrarOcultarContrasena() {
-    const inputContrasena = document.getElementById("contrasena");
-    const icono = document.getElementById("iconoOjo");
-  
-    const esOculta = inputContrasena.type === "password";
-    inputContrasena.type = esOculta ? "text" : "password";
-    icono.className = esOculta ? "bi bi-eye-slash" : "bi bi-eye";
-  }
-  
